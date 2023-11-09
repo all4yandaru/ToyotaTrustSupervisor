7b7b9d04cd1d8f568253c5b7bc6a6538f690444c
boodat12 <bastianhaloho12@gmail.com>
2023-11-09 14:08:00 +0700

    feat(#860ryka9k): Profil Navigation

---
+++ src/screen/HasilCekHarga.js
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+import HasilCekHargaComponent from '../component/section/profil/HasilCekHargaComponent';
+
+const HasilCekHarga = ({navigation}) => {
+  return (
+    <View style={{flex: 1}}>
+      <HasilCekHargaComponent navigation={navigation} />
+    </View>
+  );
+};
+
+export default HasilCekHarga;
