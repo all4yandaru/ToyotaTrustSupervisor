fe16b4ebe1d854d63054901a59116ca020e07528
Aryawicaksono <wicaksono.a12ya@gmail.com>
2023-11-14 12:03:47 +0700

    feat(#86ctxh0kb):tracking main api

--- src/screen/homeAndTracking/Tracking.js
+++ src/screen/homeAndTracking/Tracking.js
@@ -1,11 +1,42 @@
-import React, {Component} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import {Text, View} from 'react-native';
 import TrackingComponent from '../../component/section/homeAndTracking/TrackingComponent';
+import {useSelector} from 'react-redux';
+import trackingHelper from '../../apiManagement/trackingHelper';
+import {useIsFocused} from '@react-navigation/native';
 
 const Tracking = ({navigation, route}) => {
+  const {session} = useSelector(state => state.session);
+  const {account} = useSelector(state => state.account);
+  const {dataTradeIn, setDataTradeIn} = useState([]);
+  const {dataNewCar, setDataNewCar} = useState([]);
+  const isFocused = useIsFocused();
+  useEffect(() => {
+    setTimeout(() => {
+      if (session.token && account) {
+        setTradeInProcess();
+        setNewCarProcess();
+      } else {
+        navigation.replace('AuthRouting');
+      }
+    }, 1000);
+  });
+
+  const setTradeInProcess = async () => {
+    const response = await trackingHelper.tradeInHelper(session.token);
+    setDataTradeIn(response);
+  };
+  const setNewCarProcess = async () => {
+    const response = await trackingHelper.newCarHelper(session.token);
+    setDataNewCar(response);
+  };
   return (
     <View style={{flex: 1}}>
-      <TrackingComponent navigation={navigation} />
+      <TrackingComponent
+        navigation={navigation}
+        dataTradeIn={dataTradeIn}
+        dataNewCar={dataNewCar}
+      />
     </View>
   );
 };
